5647bde19a0778907fe1e63e89cf4c153e7b399b
dveleztx <dveleztx@gmail.com>
2018-07-18 11:00:20 -0700

    Added exception handling for readability

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import movie_svc
+import requests.exceptions
 
 
 def main():
@@ -17,15 +18,22 @@ def search_event_loop():
     search = "ONCE_THROUGH_LOOP"
 
     while search != "x":
-        search = input("Movie search text (x to exit): ")
-        if search != "x":
-            results = movie_svc.find_movies(search)
-            print("Found {} results.".format(len(results)))
-            for r in results:
-                print("{} -- {}".format(
-                    r.year, r.title
-                ))
-            print()
+        try:
+            search = input("Movie search text (x to exit): ")
+            if search != "x":
+                results = movie_svc.find_movies(search)
+                print("Found {} results.".format(len(results)))
+                for r in results:
+                    print("{} -- {}".format(
+                        r.year, r.title
+                    ))
+                print()
+        except ValueError:
+            print("Error: Search text is required")
+        except Exception as requests.exceptions.ConnectionError as ce:
+            print("Error: Your network is down.")
+        except Exception as x:
+            print("Unexpected error. Details: {}".format(x))
 
     print("exiting...")
 
